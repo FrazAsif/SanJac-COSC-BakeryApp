0f61cd51dc576e69ce5813e3918d6118f521cfab
FrazAsif <fraz@frazasif.com>
2022-08-09 18:10:22 -0500

    initial commit

---
+++ Bakery-App.h
@@ -0,0 +1,16 @@
+#ifndef BAKERY_APP_H
+#define BAKERY_APP
+
+class Set {
+    public:
+        Set();
+        void Parse();
+        void List();
+        void Specific();
+        void Add();
+        void Update();
+        void Delete();
+        void Report();
+};
+
+#endif
